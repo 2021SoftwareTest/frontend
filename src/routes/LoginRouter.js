15fb21b24e5a0a0450819836e51e3399a8213317
HaoningLan <546004464@qq.com>
2020-09-19 17:35:15 +0800

    fix: add prettier and eslint

--- src/routes/LoginRouter.js
+++ src/routes/LoginRouter.js
@@ -1,52 +1,59 @@
 import React from 'react';
-import {Redirect, Route} from 'react-router-dom';
+import { Redirect, Route } from 'react-router-dom';
 
 export class LoginRouter extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isAuthed: false,
-            hasAuthed: false,
-        };
-    }
-
-    checkAuth = (data) => {
-        console.log(data);
-        if (data.status >= 0) {
-            this.setState({isAuthed: true, hasAuthed: true});
-        } else {
-            localStorage.removeItem('user');
-            this.setState({isAuthed: false, hasAuthed: true});
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAuthed: false,
+      hasAuthed: false,
     };
+  }
 
+  checkAuth = (data) => {
+    console.log(data);
+    if (data.status >= 0) {
+      this.setState({ isAuthed: true, hasAuthed: true });
+    } else {
+      localStorage.removeItem('user');
+      this.setState({ isAuthed: false, hasAuthed: true });
+    }
+  };
 
-    componentDidMount() {
-        // userService.checkSession(this.checkAuth);
+  componentDidMount() {
+    // userService.checkSession(this.checkAuth);
+  }
+
+  render() {
+    // eslint-disable-next-line react/prop-types
+    const { component: Component, path = '/', exact = false, strict = false } = this.props;
+
+    console.log(this.state.isAuthed);
+
+    if (!this.state.hasAuthed) {
+      return null;
     }
 
-
-    render() {
-
-        const {component: Component, path = "/", exact = false, strict = false} = this.props;
-
-        console.log(this.state.isAuthed);
-
-        if (!this.state.hasAuthed) {
-            return null;
+    return (
+      <Route
+        path={path}
+        exact={exact}
+        strict={strict}
+        render={(props) =>
+          this.state.isAuthed ? (
+            <Redirect
+              to={{
+                pathname: '/',
+                state: { from: props.location },
+              }}
+            />
+          ) : (
+            <Component {...props} />
+          )
         }
-
-        return <Route path={path} exact={exact} strict={strict} render={(props) => (
-            this.state.isAuthed ? (
-                <Redirect to={{
-                    pathname: '/',
-                    state: {from: props.location}
-                }}/>
-            ) : (
-                <Component {...props}/>
-            )
-        )}/>;
-    }
+      />
+    );
+  }
 }
 
 export default LoginRouter;
